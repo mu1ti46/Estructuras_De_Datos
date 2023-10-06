734753cf1fe0fbcf49efd655e9ba96d80426a47c
mu1ti46 <124190480+mu1ti46@users.noreply.github.com>
2023-10-06 13:37:47 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,35 @@
-#include <Persona.hpp>
+#include <iostream>
 
-int main(int argc, char **argv)
+using namespace std;
+const int minimo = 0, maximo = 10;
+
+int main(int argc, char** argv)
 {
-	Persona *p = new Persona(18);
-    Persona *s = new Persona(19);
-    Persona *q = new Persona(20);
-    Persona *t = new Persona(21);
-    Persona *m = new Persona(22);
-    Persona *n = new Persona(23);
-    Persona *o = new Persona(24);
-    Persona *r = new Persona(25);
-    Persona *z = new Persona(26);
-    Persona *x = new Persona(27);
-    p->mostrar();
-    s->mostrar();
-    q->mostrar();
-    t->mostrar();
-    m->mostrar();
-    n->mostrar();
-    o->mostrar();
-    r->mostrar();
-    z->mostrar();
-    x->mostrar();
+    int n;
+    cout << "Introduzca un numero entre " << minimo << " y " << maximo << "." << endl;
+    cin >> n;
+    if((n < minimo) || (n > maximo))
+        cout << "El valor del " << n << " esta fuera del rango." << endl;
+    else
+        switch(n) {
+        case 0:
+            cout << "No existe definicion de primo en este caso." << endl;
+            break;
+        case 2:
+        case 3:
+        case 5:
+        case 7:
+            cout << "El numero " << n << " es primo" << endl;
+            break;
+        default:
+            cout << " El numero " << n << " no es primo." << endl;
+            break;
+        };
+        
+    return 0;
     
+    
+    
+    return 0;
 }
+
